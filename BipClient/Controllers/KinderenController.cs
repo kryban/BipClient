cc0a0bf5869a81d908945ddb25abd231dd50c1b2
bandik <bandik@wigo4it.nl>
2019-09-23 11:33:41 +0200

    Kinderen geimplementeerd

--- BipClient/Controllers/KinderenController.cs
+++ BipClient/Controllers/KinderenController.cs
@@ -15,6 +15,36 @@ namespace Bip.Controllers
         {
             client = new BipClient(new System.Net.Http.HttpClient());
         }
+        
+        // GET: Kinderen
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Zoek(string apiVersionZoek, string bsnZoek)
+        {
+            IEnumerable<IngeschrevenPersoon> ingeschrevenPersonen;
+
+            try
+            {
+                var response = client.IngeschrevenpersonenBurgerservicenummerkinderenAsync(bsnZoek, apiVersionZoek).Result;
+                IEnumerable<KindHal> result = response._embedded.Kinderen;
+
+                //var dummyLocal = DummyKind;
+                //dummyLocal.Burgerservicenummer = id.ToString();
+                //dummyLocal.Naam.Geslachtsnaam = DummyKind.Naam.Geslachtsnaam + "-" + id.ToString();
+                //IEnumerable<KindHal> result = new List<KindHal>() { dummyLocal};
+
+                return View(result);
+            }
+            catch (Exception e)
+            {
+                ViewBag.ResponseError = e.InnerException;
+                return View();
+            }
+        }
 
         private KindHal DummyKind
         {
@@ -22,36 +52,11 @@ namespace Bip.Controllers
             {
                 var retval = new KindHal();
                 retval.Burgerservicenummer = "222222221";
-                retval.Naam = new Naam() { Geslachtsnaam="Kind1", Voorletters="JJ", Voornamen="Jan" };
+                retval.Naam = new Naam() { Geslachtsnaam = "Kind1", Voorletters = "JJ", Voornamen = "Jan" };
 
                 return retval;
             }
 
-        }
-
-        // GET: Kinderen
-        public ActionResult Index()
-        {
-            var response = client.IngeschrevenpersonenBurgerservicenummerkinderenAsync("999993653", null).Result;
-            IEnumerable<KindHal> result = response._embedded.Kinderen;
-
-            //IEnumerable<KindHal> result = new List<KindHal>() { DummyKind };
-
-            return View(result);
-        }
-
-        // GET: Kinderen/Details/5
-        public ActionResult Details(int id)
-        {
-            var response = client.IngeschrevenpersonenBurgerservicenummerkinderenAsync(id.ToString(), null).Result;
-            IEnumerable<KindHal> result = response._embedded.Kinderen;
-
-            //var dummyLocal = DummyKind;
-            //dummyLocal.Burgerservicenummer = id.ToString();
-            //dummyLocal.Naam.Geslachtsnaam = DummyKind.Naam.Geslachtsnaam + "-" + id.ToString();
-            //IEnumerable<KindHal> result = new List<KindHal>() { dummyLocal};
-
-            return View(result);
         }
     }
 }
