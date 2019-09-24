9e663206102ac81ac10b814adf92012db1cb32e5
bandik <bandik@wigo4it.nl>
2019-09-24 07:18:48 +0200

    Verblijfstitelhistorie geimplementeerd

--- BipClient/Controllers/VerblijfstitelhistorieController.cs
+++ BipClient/Controllers/VerblijfstitelhistorieController.cs
@@ -16,6 +16,30 @@ namespace Bip.Controllers
             client = new BipClient(new System.Net.Http.HttpClient());
         }
 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Zoek(string bsnZoek, string apiVersionZoek, string fieldsZoek, DateTimeOffset peilDatumZoek, DateTimeOffset periodeVanZoek, DateTimeOffset periodeTotZoek)
+        {
+            var response = client.GetverblijfstitelhistorieAsync(
+                bsnZoek
+                , apiVersionZoek
+                , fieldsZoek
+                , peilDatumZoek.Year == 1 ? (DateTimeOffset?)null : peilDatumZoek
+                , periodeVanZoek.Year == 1 ? (DateTimeOffset?)null : periodeVanZoek
+                , periodeTotZoek.Year == 1 ? (DateTimeOffset?)null : periodeTotZoek).Result;
+
+            IEnumerable<VerblijfstitelhistorieHal> result = response._embedded.Verblijfstitelhistorie;
+
+            //var dummyLocal = DummyPartnerhistorie;
+            //dummyLocal.Naam.Geslachtsnaam = DummyPartnerhistorie.Naam.Geslachtsnaam + id.ToString();
+            //IEnumerable<PartnerhistorieHal> result = new List<PartnerhistorieHal>() { dummyLocal };
+
+            return View(result);
+        }
+
         private VerblijfstitelhistorieHal DummyVerblijfstitelhistorie
         {
             get
@@ -26,30 +50,5 @@ namespace Bip.Controllers
             }
 
         }
-
-        // GET: Ouders
-        public ActionResult Index()
-        {
-            var response = client.GetverblijfstitelhistorieAsync("999994669", null,null,null,null,null).Result;
-            IEnumerable<VerblijfstitelhistorieHal> result = response._embedded.Verblijfstitelhistorie;
-
-            //IEnumerable<VerblijfstitelhistorieHal> result = new List<VerblijfstitelhistorieHal>() { DummyVerblijfstitelhistorie };
-
-            return View(result);
-        }
-
-        // GET: Ouders/Details/5
-        public ActionResult Details(int id)
-        {
-            var response = client.GetverblijfstitelhistorieAsync(id.ToString(), null,null,null,null,null).Result;
-            IEnumerable<VerblijfstitelhistorieHal> result = response._embedded.Verblijfstitelhistorie;
-
-            //var dummyLocal = DummyVerblijfstitelhistorie;
-            //dummyLocal.DatumEinde = new Datum_onvolledig() { Dag = 23, Maand = 12, Jaar = 2019 };
-            //IEnumerable<VerblijfstitelhistorieHal> result = new List<VerblijfstitelhistorieHal>() { dummyLocal };
-
-            return View(result);
-        }
-
     }
 }
